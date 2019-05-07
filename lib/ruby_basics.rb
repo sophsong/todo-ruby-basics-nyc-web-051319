11e1ae8b504e5e9241b0884e607c6c54f6588e29
Sophia Song <ososanne@gmail.com>
2019-05-07 00:59:45 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,25 +1,25 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  "#{value}"
 end
 
-def argue
-  
+def argue (phrase)
+  "#{phrase}"
 end
 
-def greeting
-
+def greeting(greeting,name)
+ "#{greeting}, #{name}"
 end
 
 def return_a_value
-
+"Yup"
 end
 
 def last_evaluated_value
-
+"hi"
 end
 
 def pizza_party
